a361eab4d1282ed3ab45c61d9c7031d9d70bb642
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-03-20 08:48:24 +0900

    fixes #18

--- lib/cucumber-feed/config.rb
+++ lib/cucumber-feed/config.rb
@@ -1,5 +1,6 @@
 require 'yaml'
 require 'singleton'
+require 'cucumber-feed/package'
 
 module CucumberFeed
   class Config < Hash
@@ -20,8 +21,8 @@ module CucumberFeed
 
     def dirs
       return [
-        File.join('/usr/local/etc', File.basename(ROOT_DIR)),
-        File.join('/etc', File.basename(ROOT_DIR)),
+        File.join('/usr/local/etc', Package.name),
+        File.join('/etc', Package.name),
         File.join(ROOT_DIR, 'config'),
       ]
     end
