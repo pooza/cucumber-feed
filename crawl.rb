130de467220c47c0f37d30f474f4f54ce0f01fb4
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-08-17 17:13:08 +0900

    #24

--- crawl.rb
+++ crawl.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
-ROOT_DIR = File.expand_path(__dir__)
+path = File.expand_path(__FILE__)
+path = File.expand_path(File.readlink(path)) while File.symlink?(path)
+ROOT_DIR = File.expand_path('..', path)
 $LOAD_PATH.push(File.join(ROOT_DIR, 'lib'))
 ENV['BUNDLE_GEMFILE'] ||= File.join(ROOT_DIR, 'Gemfile')
 ENV['SSL_CERT_FILE'] ||= File.join(ROOT_DIR, 'cert/cacert.pem')
