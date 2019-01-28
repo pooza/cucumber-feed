242e5fb62f14b9bc6d1130ba9ab796daf266394a
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2019-01-28 18:49:42 +0900

    #75

--- lib/cucumber_feed/renderer/toei_feed_renderer.rb
+++ lib/cucumber_feed/renderer/toei_feed_renderer.rb
@@ -9,7 +9,7 @@ module CucumberFeed
     end
 
     def url
-      return 'http://www.toei-anim.co.jp/tv/precure/'
+      return 'http://www.toei-anim.co.jp/tv/precure/news/'
     end
 
     private
@@ -26,11 +26,11 @@ module CucumberFeed
     def entries
       unless @entries
         @entries = []
-        source.xpath('//ul[@class="news_list"]//a').each do |node|
+        source.xpath('//div[@class="m-list-news"]//a').each do |node|
           @entries.push({
             link: parse_url(node.attribute('href')).to_s,
             title: node.search('p').inner_text.gsub(/\s+/, ' ').strip,
-            date: Time.parse(node.search('span[@class="day"]').inner_text),
+            date: Time.parse(node.search('p[@class="data-date"]').inner_text),
             image: node.search('img').attribute('src').value,
           })
         end
