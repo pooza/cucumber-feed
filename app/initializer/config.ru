f268eed8e16d29b1b2e95faa132ada6e0ce757f1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-12-13 17:48:51 +0900

    #224

--- app/initializer/config.ru
+++ app/initializer/config.ru
@@ -1,6 +1,6 @@
 dir = File.expand_path('../..', __dir__)
 $LOAD_PATH.unshift(File.join(dir, 'app/lib'))
 ENV['BUNDLE_GEMFILE'] ||= File.join(dir, 'Gemfile')
-require 'cucumber_feed'
 
+require 'cucumber_feed'
 run CucumberFeed::Server
--- bin/crawl.rb
+++ bin/crawl.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
-
 path = File.expand_path(__dir__)
 path = File.expand_path(File.readlink(path)) while File.symlink?(path)
 dir = File.expand_path('..', path)
 $LOAD_PATH.unshift(File.join(dir, 'app/lib'))
 ENV['BUNDLE_GEMFILE'] = File.join(dir, 'Gemfile')
-require 'cucumber_feed'
 
+require 'cucumber_feed'
 CucumberFeed::FeedRenderer.all(&:crawl)
