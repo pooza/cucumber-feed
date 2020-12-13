fec1fd4a38e4b015109313486eafc3a688188ee0
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-12-13 17:40:27 +0900

    #224

--- bin/crawl.rb
+++ bin/crawl.rb
@@ -5,8 +5,6 @@ path = File.expand_path(File.readlink(path)) while File.symlink?(path)
 dir = File.expand_path('..', path)
 $LOAD_PATH.unshift(File.join(dir, 'app/lib'))
 ENV['BUNDLE_GEMFILE'] = File.join(dir, 'Gemfile')
-
-require 'bundler/setup'
 require 'cucumber_feed'
 
 CucumberFeed::FeedRenderer.all(&:crawl)
--- bin/puma_daemon.rb
+++ bin/puma_daemon.rb
@@ -3,8 +3,6 @@
 dir = File.expand_path('..', __dir__)
 $LOAD_PATH.unshift(File.join(dir, 'app/lib'))
 ENV['BUNDLE_GEMFILE'] = File.join(dir, 'Gemfile')
-
-require 'bundler/setup'
 require 'cucumber_feed'
 
 CucumberFeed::PumaDaemon.spawn!
