8a52d42974aaa14e0a543518eca2f374b60c57f0
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-12-13 18:20:18 +0900

    #225

--- test/garden_feed_renderer.rb
+++ test/garden_feed_renderer.rb
@@ -2,13 +2,11 @@ module CucumberFeed
   class GardenFeedRendererTest < Test::Unit::TestCase
     def setup
       @renderer = FeedRenderer.create('garden')
-      return if Environment.ci?
       @config = Config.instance
       @config['/slack/hooks'] = []
     end
 
     def test_crawl
-      return if Environment.ci?
       result = @renderer.crawl
       assert(File.exist?(result[:atom]))
       assert(File.exist?(result[:rss]))
@@ -16,7 +14,6 @@ module CucumberFeed
     end
 
     def test_to_s
-      return if Environment.ci?
       @renderer.type = 'rss'
       assert(@renderer.to_s.present?)
       assert_equal(@renderer.type, 'application/rss+xml; charset=UTF-8')
