c5d1d5d416a76bbadec3fd1bd7fa2e9c1bcf7a08
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-02-22 19:31:54 +0900

    .

--- app/lib/cucumber_feed/renderer/abc_feed_renderer.rb
+++ app/lib/cucumber_feed/renderer/abc_feed_renderer.rb
@@ -9,7 +9,7 @@ module CucumberFeed
     end
 
     def source_url
-      return 'https://www.asahi.co.jp/precure/healingood/news/'
+      return 'https://www.asahi.co.jp/precure/tropical/news/'
     end
 
     private
@@ -26,13 +26,14 @@ module CucumberFeed
     def entries
       unless @entries
         @entries = []
-        source.xpath('//ul[@class="listbox"]//a').each do |node|
+        source.xpath('//dl[@class="newswrap"]//li').each do |node|
           @entries.push({
-            link: parse_url(node.attribute('href')).to_s,
-            title: node.search('img').attribute('alt').value,
-            date: Time.parse(node.search('dd[@class="date"]').inner_text),
-            image: node.search('img').attribute('src').value,
+            link: parse_url(node.search('dd//a').attribute('href').value).to_s,
+            title: node.search('dd//a').inner_text,
+            date: Time.parse(node.search('dt').inner_text),
           })
+        rescue => e
+          @logger.error(error: e)
         end
       end
       return @entries
